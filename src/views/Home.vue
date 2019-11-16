f9e0b3a9aeb1bc9165dd98eb89120963e2dd3148
Jack Domleo <jackdomleo7@hotmail.com>
2019-11-16 18:44:49 +0000

    Added homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,90 @@
 <template>
   <div class="home">
-
+    <section class="home__hero hero">
+      <img class="hero__img" src="/img/pink-wreath.png" alt="Pink wreath" />
+      <h2 class="hero__title">Design</h2>
+    </section>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
 
-export default {
-  name: 'home',
-  components: {
+@Component
+export default class Home extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+@import "../scss/colours";
+
+.home {
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  height: 100%;
+  min-height: 80vh;
+}
+
+.hero {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  max-height: 26rem;
+  max-width: 26rem;
+
+  &__title {
+    color: $grey-700;
+    font-family: 'Sacramento', cursive;
+    font-size: 5rem;
+    margin: 0;
+    animation-delay: 1s;
+    animation-duration: 0.5s;
+    animation-fill-mode: forwards;
+    animation-name: bounceIn;
+    animation-timing-function: linear;
+    opacity: 0;
+
+    @media (max-width: 380px) {
+      font-size: 4.5rem;
+    }
+  }
+
+  &__img {
+    animation: 1s ease spin-anticlockwise;
+    position: absolute;
   }
 }
-</script>
+
+@keyframes spin-anticlockwise {
+  0% {
+    transform: rotate(180deg);
+    opacity: 0;
+  }
+  100% {
+    transform: rotate(0deg);
+    opacity: 1;
+  }
+}
+
+@keyframes bounceIn {
+  0% {
+    opacity: 0;
+    transform: scale(0.3) translate3d(0, 0, 0);
+  }
+
+  50% {
+    opacity: 0.9;
+    transform: scale(1.1);
+  }
+
+  80% {
+    opacity: 1;
+    transform: scale(0.89);
+  }
+
+  100% {
+    opacity: 1;
+    transform: scale(1) translate3d(0, 0, 0);
+  }
+}
+</style>
