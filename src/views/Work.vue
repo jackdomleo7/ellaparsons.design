4c3155fba96bfa7de02e6a3e7fcf6d2c2af66722
Jack Domleo <jackdomleo7@hotmail.com>
2020-04-26 22:59:17 +0100

    Added artwork jobs to Work page

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -1,57 +1,198 @@
 <template>
   <div class="work">
-    <section class="work__gallery">
-      <figure class="work__figure" v-for="(number, index) in mockNumber" :key="index">
-        <img class="work__image" alt="Image" src="img/pink-wreath.png" />
-      </figure>
+    <p>Below are some artwork jobs/logos I have done during my time at <site-link type="external" link="https://www.mirical-emblems.com">MiRiCal Emblems</site-link>.</p>
+    <p>For other designs and photography I have done, please see a collection of my portfolio on <site-link type="external" :link="commonMedia.pinterest.url">{{ commonMedia.pinterest.platform }}</site-link>, <site-link type="external" :link="commonMedia.instagram.url">{{ commonMedia.instagram.platform }}</site-link>, <site-link type="external" :link="commonMedia.gurushots.url">{{ commonMedia.gurushots.platform }}</site-link>, <site-link type="external" :link="commonMedia.dribbble.url">{{ commonMedia.dribbble.platform }}</site-link> and <site-link type="external" :link="commonMedia.designcrowd.url">{{ commonMedia.designcrowd.platform }}</site-link>.</p>
+    <section>
+      <ul class="work__gallery">
+        <li v-for="(image, index) in images" :key="index" :aria-setsize="images.length" :aria-posinset="index + 1">
+          <figure class="work__figure" :aria-label="image.alt" data-cooltipz-dir="top">
+            <img class="work__image" :alt="'🖼️ ' + image.alt + ' logo'" :src="'/img/work/' + image.image + '.png'" />
+          </figure>
+        </li>
+      </ul>
     </section>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import SiteLink from '@/components/SiteLink.vue';
+import CommonInfo, { IMedias } from '@/helpers/CommonInfo';
 
-@Component
+interface IWorkImage {
+  image: string;
+  alt: string;
+}
+
+@Component({
+  components: { SiteLink }
+})
 export default class Work extends Vue {
-  private mockNumber: number = 25;
+  private get commonMedia(): IMedias {
+    return CommonInfo.commonMedia;
+  }
+
+  private images: IWorkImage[] = [
+    {
+      image: 'pizza-express-pizzeria',
+      alt: 'Pizza Express Pizzeria'
+    },
+    {
+      image: 'royal-air-force-rowing',
+      alt: 'Royal Air Force Rowing'
+    },
+    {
+      image: 'liverpool-fc',
+      alt: 'Liverpool Football Club'
+    },
+    {
+      image: 'nhs-laundry',
+      alt: 'NHS Laundry'
+    },
+    {
+      image: 'spar',
+      alt: 'SPAR'
+    },
+    {
+      image: 'virgin-media-business',
+      alt: 'Virgin Media Business'
+    },
+    {
+      image: 'stagecoach',
+      alt: 'Stagecoach'
+    },
+    {
+      image: 'stihl',
+      alt: 'STIHL'
+    },
+    {
+      image: 'bamct',
+      alt: 'BAM Construction Training'
+    },
+    {
+      image: 'general-electric',
+      alt: 'General Electric'
+    },
+    {
+      image: 'stand-fast',
+      alt: 'Stand Fast'
+    },
+    {
+      image: 'juste-de-lencre',
+      alt: 'Juste de L\'encre'
+    },
+    {
+      image: 'simon-gibson',
+      alt: 'Simon Gibson Transport'
+    },
+    {
+      image: 'peter',
+      alt: 'PETER'
+    },
+    {
+      image: 'alford',
+      alt: 'ALFORD'
+    },
+    {
+      image: 'aemt',
+      alt: 'AEMT'
+    },
+    {
+      image: 'muckendork-zeiselmauer',
+      alt: 'Muckendork Zeiselmauer'
+    },
+    {
+      image: 'ville-de-mery-sur-oise',
+      alt: 'Ville de Mery Sur Oise'
+    },
+    {
+      image: 'jugend-arbeit',
+      alt: 'Jugend Arbeit'
+    },
+    {
+      image: 'ambever',
+      alt: 'Ambever'
+    },
+    {
+      image: 'i-jump-trampoline-park',
+      alt: 'I-Jump Trampoline Park'
+    },
+    {
+      image: 'm2-construction',
+      alt: 'M2 Construction'
+    },
+    {
+      image: 'charcuterie-joubert-cap-ferret',
+      alt: 'Charcuterie Joubert Cap Ferret'
+    },
+    {
+      image: 'university-of-bath',
+      alt: 'University of Bath'
+    },
+    {
+      image: 'mc2-therapeutics',
+      alt: 'MC2 Therapeutics'
+    },
+    {
+      image: 'the-new-philly',
+      alt: 'The New Philly'
+    },
+    {
+      image: 'sogielec',
+      alt: 'Sogielec'
+    },
+    {
+      image: 'air-products',
+      alt: 'Air Products'
+    },
+    {
+      image: 'karl-hugo',
+      alt: 'Karl Hugo Mechanical Engineering'
+    },
+    {
+      image: 'mmg',
+      alt: 'MMG Carrocerias'
+    },
+    {
+      image: 'dubness',
+      alt: 'DubNess'
+    }
+  ];
 }
 </script>
 
 <style lang="scss" scoped>
 .work {
-  padding: 1rem 3rem;
+  padding: 1rem 0;
+  max-width: 72em;
+  margin: 0 auto;
+  text-align: center;
+
+  @media (min-width: 48em) {
+    padding: 1rem 3rem;
+  }
 
   &__gallery {
-    display: grid;
-    grid-gap: 1.2rem;
-    grid-template: auto / 1fr;
-
-    @media (min-width: 650px) {
-      grid-template: auto / 1fr 1fr;
-    }
-
-    @media (min-width: 1000px) {
-      grid-template: auto / 1fr 1fr 1fr;
-    }
-
-    @media (min-width: 1300px) {
-      grid-template: auto / 1fr 1fr 1fr 1fr;
-    }
-
-    @media (min-width: 1900px) {
-      grid-template: auto / 1fr 1fr 1fr 1fr 1fr;
-    }
+    padding-left: 0;
+    margin: 0;
+    list-style-type: none;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
   }
 
   &__figure {
     align-items: center;
     display: flex;
     height: 14rem;
+    width: 14rem;
     justify-content: center;
+    margin: 1rem;
   }
 
   &__image {
-    height: 100%;
+    width: 100%;
+    max-height: 100%;
     object-fit: cover;
   }
 }
