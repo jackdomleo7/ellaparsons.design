260a4a4e054efe342a6afd024aa79389dbfd0f8e
Jack Domleo <jackdomleo7@hotmail.com>
2020-04-26 12:25:39 +0100

    Added Instagram, Pinterest and Dribbble to footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,11 +2,9 @@
   <footer class="footer">
     <h3 class="footer__title">Graphics-Design-Creative</h3>
     <div>
-      <site-link type="external" link="https://gurushots.com/ella.parsons/photos" aria-label="Photography on GuruShots" data-cooltipz-dir="top" class="footer__social-link">
-        <icon class="footer__social-icon" icon="camera" />
-      </site-link>
-      <site-link type="external" link="https://designers.designcrowd.co.uk/designer.aspx?designerId=842923" aria-label="Logo design on DesignCrowd" data-cooltipz-dir="top" class="footer__social-link">
-        <icon class="footer__social-icon" icon="object-group" />
+      <site-link v-for="(social, index) in socialLinks" :key="index" type="external" :link="social.link" :aria-label="social.tooltip" data-cooltipz-dir="top" class="footer__social-link">
+        <img v-if="social.image" class="footer__social-icon" :src="'/img/' + social.icon" />
+        <icon v-else class="footer__social-icon" :icon="social.icon" />
       </site-link>
     </div>
   </footer>
@@ -17,10 +15,47 @@ import { Component, Vue } from 'vue-property-decorator';
 import Icon from '@/components/Icon.vue';
 import SiteLink from '@/components/SiteLink.vue';
 
+interface ISocialLinks {
+  tooltip: string;
+  icon: string;
+  link: string;
+  image?: boolean;
+}
+
 @Component({
   components: { Icon, SiteLink },
 })
-export default class AppFooter extends Vue {}
+export default class AppFooter extends Vue {
+  private readonly socialLinks: ISocialLinks[] = [
+    {
+      tooltip: 'Pinterest',
+      icon: 'pinterest',
+      link: 'https://www.pinterest.co.uk/ellaparsons1633'
+    },
+    {
+      tooltip: 'Instagram',
+      icon: 'instagram',
+      link: 'https://www.instagram.com/ella.photos00'
+    },
+    {
+      tooltip: 'Photography on GuruShots',
+      icon: 'gurushots.png',
+      link: 'https://gurushots.com/ella.parsons/photos',
+      image: true
+    },
+    {
+      tooltip: 'Dribbble',
+      icon: 'dribbble',
+      link: 'https://dribbble.com/Ellaparsons'
+    },
+    {
+      tooltip: 'Logo design on DesignCrowd',
+      icon: 'designcrowd.png',
+      link: 'https://designers.designcrowd.co.uk/designer.aspx?designerId=842923',
+      image: true
+    }
+  ]
+}
 </script>
 
 <style lang="scss" scoped>
@@ -51,6 +86,12 @@ export default class AppFooter extends Vue {}
       margin: 0 0.5rem;
       padding: 0.3rem;
       transition: 0.2s background-color ease-in-out;
+
+      &:hover,
+      &:focus {
+        background-color: var(--color-grey-700);
+        color: var(--color-grey-50);
+      }
     }
 
     &-icon {
