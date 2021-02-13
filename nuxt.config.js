54333c5983243a84991ab5ed68296ca50a73088e
Jack Domleo <jackdomleo7@gmail.com>
2021-02-13 22:52:25 +0000

    Changed titleTemplate to title due to the website being a single page

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s | Ella Parsons - Designer & Photographer',
+    title: 'Ella Parsons - Designer & Photographer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
